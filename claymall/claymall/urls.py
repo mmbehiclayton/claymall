6bfc95e403c53d75cd9d7e47553df0e7d1c31d78
Clayton Mmbehi <30318074+mmbehiclayton@users.noreply.github.com>
2023-07-19 22:39:59 +0300

    update cart

--- claymall/claymall/urls.py
+++ claymall/claymall/urls.py
@@ -3,10 +3,18 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
+    #urls for the admin site
  path('admin/', admin.site.urls),
+
+ #register urls for the cart app
+ path('cart/', include('cart.urls', namespace='cart')),
+
+ #register urls for the shop app
  path('', include('shop.urls', namespace='shop')),
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
